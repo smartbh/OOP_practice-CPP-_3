c4765bc0aa2fd7def021cf8ccfedf6e67dda6898
smartbh <smartbh@naver.com>
2021-08-23 20:39:50 +0900

    basig CPP practice

---
+++ test/Box.h
@@ -0,0 +1,15 @@
+#ifndef BOX_H
+#define BOX_H
+
+class Box {
+private:
+	int width, height;
+	char fill; //박스의 내부를 채우는 문자
+public:
+	Box(int w, int h);
+	void setFill(char f);
+	void setSize(int w, int h);
+	void draw(); //박스 그리기
+};
+
+#endif
